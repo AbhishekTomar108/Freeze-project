06958844e60de59afe344b8e5bcef641f4342de2
AbhishekTomar108 <abhishektomar9211@gmail.com>
2023-06-22 01:13:19 -0700

    test 2 added with pyautogui

--- test2.py
+++ test2.py
@@ -1,26 +1,26 @@
 from flask import Flask
-# import pyautogui
+import pyautogui
 import keyboard
 
-# keyboard.block_key('Win')
-# keyboard.block_key('tab')
-# keyboard.block_key('ctrl')
-# keyboard.block_key('enter')
-# keyboard.block_key('shift')
-# keyboard.block_key('f1')
-# keyboard.block_key('f2')
-# keyboard.block_key('f3')
-# keyboard.block_key('f5')
-# keyboard.block_key('f4')
-# keyboard.block_key('f6')
-# keyboard.block_key('f7')
-# keyboard.block_key('f8')
-# keyboard.block_key('f9')
-# keyboard.block_key('f10')
+keyboard.block_key('Win')
+keyboard.block_key('tab')
+keyboard.block_key('ctrl')
+keyboard.block_key('enter')
+keyboard.block_key('shift')
+keyboard.block_key('f1')
+keyboard.block_key('f2')
+keyboard.block_key('f3')
+keyboard.block_key('f5')
+keyboard.block_key('f4')
+keyboard.block_key('f6')
+keyboard.block_key('f7')
+keyboard.block_key('f8')
+keyboard.block_key('f9')
+keyboard.block_key('f10')
 
 
-# pyautogui.FAILSAFE = False  # Disable the fail-safe mechanism
-# maxX, maxY = pyautogui.size()
+pyautogui.FAILSAFE = False  # Disable the fail-safe mechanism
+maxX, maxY = pyautogui.size()
 
 app = Flask(__name__)
 
@@ -45,7 +45,8 @@ def freeze_input():
     keyboard.press_and_release('f11')
     keyboard.block_key('f11')
 
-    
+    while True:
+     pyautogui.moveTo(maxX/2, maxY/2)
         
      
     html_response = """
